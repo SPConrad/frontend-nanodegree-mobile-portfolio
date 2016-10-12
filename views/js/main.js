fcddb82d08a1da5993efd8689ec01da51e48d3c9
Konrad9 <konrad9@gmail.com>
2016-10-12 16:09:46 -0400

    More fiddling to increase pagespeed score

--- views/js/main.js
+++ views/js/main.js
@@ -400,7 +400,7 @@ var pizzaElementGenerator = function(i) {
 
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
 var resizePizzas = function(size) {
-  window.performance.mark("mark_start_resize");   // User Timing API function
+  //window.performance.mark("mark_start_resize");   // User Timing API function
 
   // Changes the value for the size of the pizza above the slider
   function changeSliderLabel(size) {
@@ -463,13 +463,13 @@ var resizePizzas = function(size) {
   changePizzaSizes(size);
 
   // User Timing API is awesome
-  window.performance.mark("mark_end_resize");
-  window.performance.measure("measure_pizza_resize", "mark_start_resize", "mark_end_resize");
-  var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
-  console.log("Time to resize pizzas: " + timeToResize[timeToResize.length-1].duration + "ms");
+  //window.performance.mark("mark_end_resize");
+ // window.performance.measure("measure_pizza_resize", "mark_start_resize", "mark_end_resize");
+  //var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
+  //console.log("Time to resize pizzas: " + timeToResize[timeToResize.length-1].duration + "ms");
 };
 
-window.performance.mark("mark_start_generating"); // collect timing data
+//window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
 var pizzasDiv = document.getElementById("randomPizzas");
@@ -478,32 +478,32 @@ for (var i = 2; i < 100; i++) {
 }
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
-window.performance.mark("mark_end_generating");
+/*window.performance.mark("mark_end_generating");
 window.performance.measure("measure_pizza_generation", "mark_start_generating", "mark_end_generating");
 var timeToGenerate = window.performance.getEntriesByName("measure_pizza_generation");
 console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "ms");
 
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
-var frame = 0;
+var frame = 0;*/
 
 // Logs the average amount of time per 10 frames needed to move the sliding background pizzas on scroll.
-function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
+/*function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
   var numberOfEntries = times.length;
   var sum = 0;
   for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
     sum = sum + times[i].duration;
   }
   console.log("Average scripting time to generate last 10 frames: " + sum / 10 + "ms");
-}
+}*/
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
-  frame++;
-  window.performance.mark("mark_start_frame");
+  //frame++;
+  //window.performance.mark("mark_start_frame");
 
   ///none of these need to be declared and set inside the for loop
   var items = document.querySelectorAll('.mover');
@@ -522,40 +522,16 @@ function updatePositions() {
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
-  window.performance.mark("mark_end_frame");
+  /*window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
   if (frame % 10 === 0) {
     var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
     logAverageFrame(timesToUpdatePosition);
-  }
+  }*/
 }
 
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var col = 0;
-  var numOfColumns = 7;
-  var s = 256;
-  ///no need to have 200, no more than 30 - 40 are visible depending on browser
-  ///size, went with a few extra just in case. 
-  for (var i = 0; i < 42; i++) {
-    if ((i + 1) % numOfColumns == 0)
-    {
-      col++;
-    }
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    ///due to transformX being used I now assign this value directly to the element, instead of 
-    ///a reference value. This allows the use of transformX rather than updating style.left 
-    ///every time the element is moved. 
-    elem.style.left = s * (i % numOfColumns) + 'px';
-    elem.style.top = (col * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  updatePositions();
-});
+
 
